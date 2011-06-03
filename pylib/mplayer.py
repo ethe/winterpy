6a1ed3b0f8868fdc22deea2bf481735515fd78ca
lilydjwg <lilydjwg@gmail.com>
2011-06-04 00:09:18 +0800

    mplayer 添加 __init__ 方法

--- pylib/mplayer.py
+++ pylib/mplayer.py
@@ -1,7 +1,23 @@
 #!/usr/bin/env python3
 # vim:fileencoding=utf-8
 
+import os
+import stat
+
+from subprocessio import Subprocess, PIPE
+
 class MPlayer:
+  def __init__(self, fifo=os.path.expanduser('~/.mplayer/fifo')):
+    self.fifo = fifo
+    if not os.path.exists(self.fifo):
+      os.mkfifo(fifo, 0o600)
+    if not stat.S_ISFIFO(os.stat(fifo).st_mode):
+      raise MplayerError('文件存在但是不是 FIFO：%s' % fifo)
+    cmd = ['mplayer', '-nolirc', '-idle', '-really-quiet',
+           '-input', fifo,
+          '-input', 'default-bindings']
+    self.sub = Subprocess(cmd, stdin=PIPE, stdout=PIPE)
+
   def docmd(self, cmd):
     if not os.path.exists(self.fifo):
       raise MplayerError('FIFO 文件丢失')
@@ -10,9 +26,9 @@ class MPlayer:
     self.playing = True
     if not cmd.endswith('\n'):
       cmd += '\n'
-    f = open(self.fifo, 'w')
-    f.write(cmd)
-    f.close()
+    with open(self.fifo, 'w') as f:
+      f.write(cmd)
     time.sleep(0.2)
     return self.sub.output()
 
+class MplayerError(Exception): pass
